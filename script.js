1e30a41a63c6adc417cc7afe9d5a036164877932
mdwiltfong <wiltfong.michael1@gmail.com>
2023-10-22 16:40:16 -0400

    adding template and todo form to html

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-document.getElementById("demo").innerHTML = "Hello JavaScript";
+/*
+Project memory. This will contain all the todos that are created by the user.
+*/
+const todos = [];
+
+function addTodo(event) {
+  event.preventDefault();
+  const todoTitle = event.target[0].value;
+  const todoNumOfPeople = event.target[1].value;
+  const todoDate = event.target[2].value;
+  const todoDescription = event.target[3].value;
+  const todo = {
+    todoTitle,
+    todoNumOfPeople,
+    todoDate,
+    todoDescription,
+  };
+  todos.push(todo);
+}
+
+document.getElementById("submitTodo").addEventListener("submit", addTodo);
