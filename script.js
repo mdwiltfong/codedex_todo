54c449be27640b3731078f27297c085b65a091c3
mdwiltfong <wiltfong.michael1@gmail.com>
2023-10-22 22:19:43 -0400

    generate HTML function and render todos

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /*
 Project memory. This will contain all the todos that are created by the user.
 */
-const todos = [];
-
-function addTodo(event) {
+let todos = [];
+let todoListElement = document.getElementById("todo-list");
+function addTodo(event, todos) {
   event.preventDefault();
   const todoTitle = event.target[0].value;
   const todoNumOfPeople = event.target[1].value;
@@ -15,7 +15,37 @@ function addTodo(event) {
     todoDate,
     todoDescription,
   };
-  todos.push(todo);
+  const newTodos = [...todos, todo];
+  return newTodos;
 }
 
-document.getElementById("submitTodo").addEventListener("submit", addTodo);
+function renderTodos(todos) {
+  todoListElement.innerHTML = "";
+  todos.forEach((todo) => {
+    const newTodoHTML = generateTodoHTML(todo);
+    todoListElement.appendChild(newTodoHTML);
+  });
+
+  return newTodoDiv;
+}
+
+function generateTodoHTML(todo) {
+  const newTodoDiv = document.createElement("div");
+  newTodoDiv.className = "card todo";
+  newTodoDiv.innerHTML = `
+   <div class="card-body">
+                        <h5 class="card-title">${todo.todoTitle}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${todo.todoDate}</h6>
+                        <h6 class="card-subtitle mb-2 text-muted">${todo.todoNumOfPeople}</h6>
+                        <p class="card-text">${todo.todoDescription}</p>
+                        <button class="btn btn-primary">Done</button>
+                        <button class="btn btn-danger">Delete</button>
+                    </div>
+    `;
+  return newTodoDiv;
+}
+
+document.getElementById("submitTodo").addEventListener("submit", (event) => {
+  todos = addTodo(event, todos);
+  renderTodos(todos);
+});
