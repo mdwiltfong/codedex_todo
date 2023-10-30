6a97d8ebf9c9a8e548e63bf6225b0eba4b4270a4
mdwiltfong <wiltfong.michael1@gmail.com>
2023-10-29 22:40:57 -0400

    updateTodo function

--- script.js
+++ script.js
@@ -29,29 +29,48 @@ function addTodo(event, todos) {
   const newTodos = [...todos, todo];
   return newTodos;
 }
+function updateTodo(event, todos, todoId) {
+  event.preventDefault();
+  const todoTitle = event.target[0].value;
+  const todoNumOfPeople = event.target[2].value;
+  const todoDate = event.target[1].value;
+  const todoDescription = event.target[3].value;
+  const isEditting = false;
+  const todo = {
+    todoTitle,
+    todoNumOfPeople,
+    todoDate,
+    todoDescription,
+    isEditting,
+  };
+  todos[todoId] = todo;
+  return todos;
+}
 
 function renderTodos(todos) {
   const todoListHTML = todos
     .map((todo, index) => generateTodoHTML(todo, index))
     .join("");
-  editForm.innerHTML = todoListHTML;
+  todoListElement.innerHTML = todoListHTML;
 }
 
 function generateTodoHTML(todo, index) {
   let todoHtml = "";
   if (todo.isEditting == true) {
     todoHtml = `
-    <form>
+
     <form data-se=${index} class="card-body d-flex flex-column my-2">
-                        <input class="card-title" value=${todo.todoTitle}></input>
-                        <input type ="date" class="card-subtitle mb-2 text-muted" value=${todo.todoDate}></input>
-                        <input type="number"class="card-subtitle mb-2 text-muted" value=${todo.todoNumOfPeople}></input>
-                        <input class="card-text" value=${todo.todoDescription}></input>
-                        
-    </form>
-    <button type="submit" id="done" class="btn btn-success">Done</button>
-    <button type="submit" id="cancel" class="btn btn-primary">Cancel</button>       
-    </form>`;
+      <input class="card-title" value=${todo.todoTitle}></input>
+      <input type ="date" class="card-subtitle mb-2 text-muted" value=${todo.todoDate}></input>
+      <input type="number"class="card-subtitle mb-2 text-muted" value=${todo.todoNumOfPeople}></input>
+      <input class="card-text" value=${todo.todoDescription}></input>              
+    <div class="d-flex justify-content-start my-2">
+    <button type="submit" id="done" class="btn btn-success w-25">Done</button>
+    <button type="submit" id="cancel" class="btn btn-primary w-25 mx-2">Cancel</button>   
+    </div>
+    </form> 
+    
+    `;
   } else {
     todoHtml = `
    <div data-se=${index} class="card-body my-2">
@@ -59,8 +78,8 @@ function generateTodoHTML(todo, index) {
                         <h6 class="card-subtitle mb-2 text-muted">${todo.todoDate}</h6>
                         <h6 class="card-subtitle mb-2 text-muted">${todo.todoNumOfPeople}</h6>
                         <p class="card-text">${todo.todoDescription}</p>
-                        <button type="submit" id="done" class="btn btn-success">Done</button>
-                        <button type="submit" id="edit" class="btn btn-primary">Edit</button>
+                        <button type="click" id="done" class="btn btn-success w-25">Done</button>
+                        <button type="submit" id="edit" class="btn btn-primary w-25">Edit</button>
                     </div>
     `;
   }
@@ -80,22 +99,18 @@ document.getElementById("submitTodo").addEventListener("submit", (event) => {
 });
 document.getElementById("todo-list").addEventListener("submit", (event) => {
   event.preventDefault();
+
+  const todo = event.currentTarget;
+  const todoId = todo.getAttribute("data-se");
+
   if (event.target.id == "edit") {
-    const todo = event.target.parentElement;
-    const todoId = todo.getAttribute("data-se");
     todos = makeTodoEditable(todos, todoId);
     renderTodos(todos);
   }
-});
-document.getElementById("editForm").addEventListener("submit", (event) => {
-  event.preventDefault();
-  const todo = {
-    todoTitle: document.getElementById("todo-title").value,
-    todoDate: document.getElementById("todo-date").value,
-    todoNumOfPeople: document.getElementById("numofpeople").value,
-    todoDescription: document.getElementById("todo-description").value,
-  };
-  todos.push(todo);
-  renderTodos(todos);
-  document.getElementById("submitTodo").reset();
+
+  if (event.target.id == "done") {
+    // Update the todo here and rerender the list
+    todos = updateTodo(event, todos, todoId);
+    renderTodos(todos);
+  }
 });
