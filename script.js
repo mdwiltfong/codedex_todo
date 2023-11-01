30a95a199e47a9ca2855076effb7e38f062818a2
mdwiltfong <wiltfong.michael1@gmail.com>
2023-10-31 22:07:30 -0400

    remove updateTodo function

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ function addTodo(event, todos) {
   const todoNumOfPeople = event.target[1].value;
   const todoDate = event.target[2].value;
   const todoDescription = event.target[3].value;
-
   const todo = {
     todoTitle,
     todoNumOfPeople,
@@ -27,23 +26,6 @@ function addTodo(event, todos) {
   };
   const newTodos = [...todos, todo];
   return newTodos;
-}
-function updateTodo(event, todos, todoId) {
-  event.preventDefault();
-  const todoTitle = event.target[0].value;
-  const todoNumOfPeople = event.target[2].value;
-  const todoDate = event.target[1].value;
-  const todoDescription = event.target[3].value;
-  const isEditting = false;
-  const todo = {
-    todoTitle,
-    todoNumOfPeople,
-    todoDate,
-    todoDescription,
-    isEditting,
-  };
-  todos[todoId] = todo;
-  return todos;
 }
 
 function renderTodos(todos) {
@@ -55,17 +37,33 @@ function renderTodos(todos) {
 
 function generateTodoHTML(todo, index) {
   return `
-   <div data-se=${index} class="card-body my-2 px-2 border dorder-dark">
-                        <h5 class="card-title">${todo.todoTitle}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${todo.todoDate}</h6>
-                        <h6 class="card-subtitle mb-2 text-muted">${todo.todoNumOfPeople}</h6>
-                        <p class="card-text">${todo.todoDescription}</p>
-                        <button type="click" id="done" class="btn btn-success">Done</button>
-                    </div>
+   <div data-se=${index} class="card-body my-2 p-2 border dorder-dark">
+    <h5 class="card-title">${todo.todoTitle}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${todo.todoDate}</h6>
+    <h6 class="card-subtitle mb-2 text-muted">${todo.todoNumOfPeople}</h6>
+    <p class="card-text">${todo.todoDescription}</p>
+    <button type="click" id="done" class="btn btn-success">Done</button>
+    </div>
+</div>    
     `;
+}
+
+function completeTodo(todos, todoId) {
+  todos.splice(todoId, 1);
+  return todos;
 }
 
 document.getElementById("submitTodo").addEventListener("submit", (event) => {
   todos = addTodo(event, todos);
   renderTodos(todos);
+});
+
+document.getElementById("todo-list").addEventListener("click", (event) => {
+  if (event.target.id === "done") {
+    todos = completeTodo(
+      todos,
+      event.target.parentElement.getAttribute("data-se")
+    );
+    renderTodos(todos);
+  }
 });
